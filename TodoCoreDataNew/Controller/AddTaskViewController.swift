f913cd5fffcb438c32d408242e335bb5d577ae19
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-13 15:56:53 +0500

    Somemore work done

---
+++ TodoCoreDataNew/Controller/AddTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AddTaskViewController.swift
+//  TodoCoreDataNew
+//
+//  Created by Elexoft on 13/12/2024.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+
+}
