cd08547c223c4b438c90f857bf91916a4ef3eea9
Bangash38 <bangashmhasnain38@gmail.com>
2024-12-15 14:48:36 +0500

    Toggle button saving in the core data work done

--- TodoCoreDataNew/View/Task Cell/TaskCell.swift
+++ TodoCoreDataNew/View/Task Cell/TaskCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class TaskCell: UITableViewCell {
     
@@ -14,10 +15,12 @@ class TaskCell: UITableViewCell {
     @IBOutlet weak var taskTitleDescription: UILabel!
     @IBOutlet weak var taskToggleButton: UISwitch!
     
+    var task: TaskToDo?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        taskToggleButton.isOn = false
+        // taskToggleButton.isOn = task?.isCompleted ?? false
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -26,25 +29,33 @@ class TaskCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
-    
-    @IBAction func taskToggleButtonPressed(_ sender: UISwitch) {
-        if sender.isOn {
-            // Add a strike-through
+    func toggleButtonStrikeThrough(state: Bool) {
+        if state {
             let attributedString = NSMutableAttributedString(string: taskTitle.text ?? "")
             attributedString.addAttribute(.strikethroughStyle,
                                           value: NSUnderlineStyle.single.rawValue,
                                           range: NSRange(location: 0, length: attributedString.length))
             taskTitle.attributedText = attributedString
         } else {
-            // Remove the strike-through
             taskTitle.attributedText = NSAttributedString(string: taskTitle.text ?? "")
         }
     }
     
+    @IBAction func taskToggleButtonPressed(_ sender: UISwitch) {
+        task?.isCompleted = sender.isOn
+        PersistentStorage.shared.saveContext()
+        toggleButtonStrikeThrough(state: sender.isOn)
+    }
+    
     func configureCell(with task: TaskToDo) {
+        self.task = task
         taskTitle.text = task.taskTitle
         taskTitleDescription.text = task.taskText
+        taskToggleButton.isOn = task.isCompleted
         
+        // toggleButtonStrikeThrough(state: taskToggleButton.isOn)
+        
+        // Set the task priority view color based on the task's priority
         switch task.taskPriority {
         case "Low":
             taskPriorityView.backgroundColor = UIColor.yellow
@@ -56,5 +67,4 @@ class TaskCell: UITableViewCell {
             taskPriorityView.backgroundColor = UIColor.clear
         }
     }
-    
 }
